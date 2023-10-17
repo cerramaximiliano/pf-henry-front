3fda15ccee305e5dcb5a667382bb72d33be9ded7
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2023-10-17 14:06:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Profile from "./views/Myaccount/Myaccount";
 import Us from "./views/Aboutus/us";
 import Cart from "./views/cart/cart";
 import NavBar from "./components/NavBar/navBar";
-import Landing from "./views/Landing/Landing";
+import Landing from "./views/landing/landing";
 
 
 function App() {
