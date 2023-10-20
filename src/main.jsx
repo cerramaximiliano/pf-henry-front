047b68f79b0baa2721a105efcec431f4104034d2
WVallejos <guille.vallejosr@gmail.com>
2023-10-20 11:25:16 -0300

    addlogin

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,17 @@ import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import "./index.css";
 import FiltersProvider from "./context/filter.jsx";
+import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
  
     <BrowserRouter> 
     <Provider store={store}>
+      <Auth0ProviderWithNavigate>
       <FiltersProvider>
       <App /> 
       </FiltersProvider>
+      </Auth0ProviderWithNavigate>
       </Provider>
     </BrowserRouter>
  
