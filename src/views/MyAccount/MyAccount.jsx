3a35ce2d07c839d2d892686ae3480c3c0c2964a6
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2023-10-22 15:00:59 -0300

    deploy

--- src/views/MyAccount/MyAccount.jsx
+++ src/views/MyAccount/MyAccount.jsx
@@ -1,9 +1,9 @@
 import Dashboard from "../../components/Dashboard/Dashboard"
 
-export default function MyAccount (){
+export default function MyAccount() {
     return (
-        <>
-            <Dashboard/>
-        </>
+        <div>
+            <Dashboard />
+        </div>
     )
 }
