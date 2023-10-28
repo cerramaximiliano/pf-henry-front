0400b2a8f810d626a2f45245b9dfca11299f0779
cerramaximiliano <cerramaximiliano@gmail.com>
2023-10-27 21:37:25 -0300

    feature/responsive-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,71 +2,72 @@ import logo from "../../assets/Logo(1).png";
 
 export function Footer() {
     return (
-        <div className="mx-auto">
-            <div className="
-            sm:h-[650px]
-            relative bg-whiteFred-100 h-[250px] text-left text-xl text-blackFred-100">
-                {/* Botón de suscripción */}
-                <button className="relative bg-redFred-300 text-blackFred-300 py-[8px] px-[28px] font-bebas rounded-sm">
-                    submit
-                </button>
+        <div className="flex flex-row flex-wrap">
+            <div className="flex flex-row flex-1 justify-around"> 
+                <div className="">
+                    <p className="font-impact  leading-[32px] text-orangeFred-300">DEVELOPERS</p>
+                    <div className="flex flex-row justify-around">
+                    <div>
+                        <p className="hover:text-redFred-100 font-inter text-blackFred-100 cursor-pointer">@gmail.com</p>
+                        <p className="hover:text-redFred-100 font-inter text-blackFred-100 cursor-pointer">@gmail.com</p>
+                        <p className="hover:text-redFred-100 font-inter text-blackFred-100 cursor-pointer">@gmail.com</p>
+                        <p className="hover:text-redFred-100 font-inter text-blackFred-100 cursor-pointer">@gmail.com</p>
+                    </div>
+                    <div>
+                        <p className="hover:text-redFred-100 font-inter text-blackFred-100 cursor-pointer">@gmail.com</p>
+                        <p className="hover:text-redFred-100 font-inter text-blackFred-100 cursor-pointer">@gmail.com</p>
+                        <p className="hover:text-redFred-100 font-inter text-blackFred-100 cursor-pointer">@gmail.com</p>
+                        <p className="hover:text-redFred-100 font-inter text-blackFred-100 cursor-pointer">@gmail.com</p>
+                    </div>
+                    </div>
 
-                {/* Título de la sección de suscripción */}
-                <p className="relative font-impact leading-[32px] text-orangeFred-300">
-                    NEWSLETTER
-                </p>
 
-                {/* Formulario de suscripción */}
-                <div>
-                    <input
-                        className="relative  bg-whiteFred-100 py-[8.5px] px-[24px] font-bebas rounded-sm w-[320px]"
-                        placeholder="E-MAIL"
-                    />
-                    {/* Botones de redes sociales */}
-                    <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
-                    <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
-                    <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
-                    <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
-                    <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
-                    <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
-                    <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
-                    <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
                 </div>
-
+                <div className="">
+                    <p className="font-impact  leading-[32px] text-orangeFred-300">LINKS</p>
+                    <p className="font-impact">
+                        <a className="text-blackFred-300 hover:text-orangeFred-300" href="/home">Home</a>
+                    </p>
+                    <p className="font-impact">
+                        <a className="text-blackFred-300 hover:text-orangeFred-300" href="/products">Products</a>
+                    </p>
+                    <p className="font-impact">
+                        <a className="text-blackFred-300 hover:text-orangeFred-300" href="">Pay method</a>
+                    </p>
+                    <p className="font-impact">
+                        <a className="text-blackFred-300 hover:text-orangeFred-300" href="/aboutus">About us</a>
+                    </p>
+                </div>
+            </div>
+            <div className="flex flex-col flex-1">
                 <div>
-                    {/* Enlaces de contacto y navegación */}
-                    <p className="relative font-impact  leading-[32px] text-orangeFred-300">
-                        CONTACT
-                    </p>
-                    <p className="relative font-impact  leading-[32px] text-orangeFred-300">
-                        USEFUL LINE
-                    </p>
-                    <p className="relative font-impact  leading-[32px] text-orangeFred-300">
-                        DEVELOPERS
-                    </p>
-
-                    {/* Contenido adicional */}
-                    <p className="relative transition duration-400 ease-in-out hover:text-redFred-100 hover:text-9xl  font-inter text-blackFred-100 leading-[32px]">
-                        Landing
-                    </p>
-                    <p className="relative transition duration-400 ease-in-out hover:text-redFred-100 hover:text-9xl  font-inter text-blackFred-100 leading-[32px]">
-                        @gmail.com
-                    </p>
-                    {/* Otros elementos como enlaces, direcciones de correo electrónico, etc. */}
-
-                    {/* Logo de la empresa */}
-                    <img
-                        className="relative w-[250px] h-[180px] object-fit"
-                        alt=""
-                        src={logo}
-                    />
-
-                    {/* Derechos de autor */}
+                    <img className=""   alt=""  src={logo}/>
+                </div>
+                <div>
                     <div className="relative leading-[32px] text-blackFred-100">
-                        ©2023 GENSHYSUPLEMENTS. Todos los derechos reservados.
+                        <p className="font-impact">©2023 GENSHYSUPLEMENTS. Todos los derechos reservados.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col flex-1">
+                <div>
+                    <div>
+                        <p className="font-impact  leading-[32px] text-orangeFred-300">NEWSLETTER</p>
+                    </div>
+                    <div className="flex justify-around">
+                        <input className="relative  bg-whiteFred-100 py-[8.5px] px-[24px] font-bebas rounded-sm w-[200px] mt-6" placeholder="E-MAIL"/>
+                        <button className="relative bg-redFred-300 text-blackFred-300 py-[8px] px-[28px] font-bebas rounded-sm mt-6">submit</button>
+                    </div>
+                </div>
+                <div className="mt-6">
+                    <div className="flex justify-around">
+                        <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
+                        <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
+                        <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
+                        <button className="relative rounded-sm transition transform hover:scale-110 bg-redFred-100 w-[54px] h-[54px]" />
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
