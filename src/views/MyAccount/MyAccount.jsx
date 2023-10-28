e5896a26f296d38a4987504d0bc6227f424d5b26
cerramaximiliano <cerramaximiliano@gmail.com>
2023-10-28 12:02:13 -0300

    feature/myaccount

--- src/views/MyAccount/MyAccount.jsx
+++ src/views/MyAccount/MyAccount.jsx
@@ -1,28 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import Dashboard from "../../components/Dashboard/Dashboard";
+import UserMyAccount from "../../components/UserMyAccount/UserMyAccount";
 import axios from 'axios';
 import { useEffect, useState } from "react";
 
 export default function MyAccount() {
-    const {id} = useParams();
-    const URLBASE = import.meta.env.VITE_URL_BASE;
-    const [orders, setOrders] = useState([]);
-    const [date, setDate] = useState("");
-    const [status, setStatus] = useState("");
-
-    useEffect( () => {
-
-            const data = axios(`${URLBASE}/orders/update/${id}`)
-            .then(({data}) => {
-                setDate(data.order.createdAt)
-                setStatus(data.order.status)
-                setOrders([...orders, ...data.order.products])
-            })
-            .catch(err => console.log(err))
-      }, [id])
+    const { pathname } = useLocation();
     return (
         <div>
-            <Dashboard orders={orders} orderId={id} date={date} status={status}/>
+            <h3>My Account</h3>
+            {   pathname.startsWith('/myaccount/users') && <UserMyAccount/>  }
+            {   pathname.startsWith('/myaccount/orders') && <Dashboard/>  }
         </div>
     )
 }
