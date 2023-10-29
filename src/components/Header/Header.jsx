418ba5a82c884b83f2a33b37174341828f1475f7
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2023-10-29 01:18:27 -0300

    falta carrousel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import bg from "../../assets/background-pf.png";
-import spam from "../../assets/SpamCalite.png";
 
 export default function Header() {
   const [fallAnimation, setFallAnimation] = useState(true);
@@ -13,7 +12,7 @@ export default function Header() {
   }, []);
 
   return (
-    <div className="relative flex flex-col m-auto text-center bg-blackFred-300 w-full xl:h-[850px] sm:h-[800px] py-5 text-whiteFred-100 font-impact">
+    <div className={`relative flex flex-col m-auto text-center bg-blackFred-300 w-full xl:h-[850px] sm:h-[800px] py-5 text-whiteFred-100 font-impact ${fallAnimation ? "animate-fall" : ""}`}>
       <div className="sm:flex-row xl:flex-col flex flex-col items-center">
         <img className="bg-contain bg-center xl:min-w-[auto] sm:h-auto max-w-full xl:block sm:mt-[200px]" alt="" src={bg} />
         <div className="sm:ml-4 xl:ml-0 absolute inset-0">
@@ -34,9 +33,11 @@ export default function Header() {
               <p className="sm:text-9xl xl:text-31xl sm:my-0 xl:my-0 text-whiteFred-100 font-impact">STORE</p>
             </div>
           </div>
-          <button to="/products" className="bg-redFred-300 font-impact sm:text-[25px] rounded-xl shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)_inset] w-full sm:w-[250px] xl:w-[300px] h-16 mt-6 sm:mt-[100px] xl:ml-[1100px] xl:text-21xl xl:mt-[300px] mx-auto">
+          <NavLink to="/products">
+          <button className="bg-redFred-300 font-impact sm:text-[25px] rounded-xl shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)_inset] w-full sm:w-[250px] xl:w-[300px] h-16 mt-6 sm:mt-[100px] xl:ml-[1100px] xl:text-21xl xl:mt-[300px] mx-auto">
             <p className="xl:-mt-[20px] sm:-mt-[2px]">shop now</p>
           </button>
+          </NavLink>
         </div>
       </div>
     </div>
