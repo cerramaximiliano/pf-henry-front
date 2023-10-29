2da04ca2e6f309dc66183fe11f5eae43ea067e3f
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2023-10-28 21:06:26 -0300

    prueba

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,22 +13,18 @@ export default function Header() {
   }, []);
 
   return (
-    <div className="relative bg-blackFred-300 w-full text-center text-whiteFred-100 font-impact">
-      <div className="flex flex-col sm:flex-row xl:flex-col items-center justify-center">
-        <img className="max-w-full xl:h-[55em]" alt="" src={bg} />
-        <div className="sm:ml-4 xl:ml-0 absolute inset-0 flex flex-col items-center justify-center">
-          <div className="text-3xl sm:text-5xl xl:text-6xl">
-            YOUR NUTRITION FOREVER
+    <div className="relative flex flex-col m-auto text-center bg-blackFred-300 w-full sm:h-[900px] py-5 text-whiteFred-100 font-impact">
+      <div className="sm:flex-row xl:flex-col flex flex-col items-center ">
+        <img className="bg-contain bg-center h-[900px] sm:h-auto max-w-full xl:block sm:mt-[200px]" alt="" src={bg} />
+        <div className="sm:ml-4 xl:ml-0 absolute inset-0">
+          <div className="sm:text-center xl:text-center">
+            <p className="sm:text-9xl xl:text-31xl">BEST SUPPLEMENT SELLERS</p>
+            <p className="sm:text-9xl xl:text-31xl">YOUR NUTRITION FOREVER</p>
+            <p className="sm:text-9xl xl:text-31xl">ONLINE SUPPLEMENTS STORE</p>
           </div>
-          <div className=" flex flex-col " >
-            <h1 className=" flex flex-col text-3xl sm:text-5xl xl:text-6xl">BEST SUPPLEMENT SELLERS</h1>
-          </div>
-          <NavLink to="/products" className="bg-redFred-300 rounded-xl shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)_inset] w-full sm:w-[250px] xl:w-[300px] h-16 mt-6 sm:mt-4 xl:mt-6 mx-auto">shop now</NavLink>
-          <div className="text-3xl sm:text-5xl xl:text-6xl mt-4 sm:mt-0 xl:mt-4">
-            ONLINE SUPPLEMENTS STORE
-          </div>
+          <button to="/products" className="bg-redFred-300 rounded-xl shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)_inset] w-full sm:w-[250px] xl:w-[300px] h-16 mt-6 sm:mt-[350px] xl:mt-6 mx-auto">shop now</button>
         </div>
       </div>
     </div>
   );
-}
+}  
