e1e18f1a1e3ba74595f003b8a3817b0e6b5874e5
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2023-10-30 12:30:58 -0300

    mateo

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./products/productSlice";
 import usersReducer from './users/userSlice';
 import cartReducer from "./Cart/cartSlice";
-import reviewsReducer from "./reviews/reviewsSlice";
+import reviewsReducer from "./Reviews/reviewsSlice";
 import ordersReducer from "./Orders/orderSlice"
 
 export default configureStore({
