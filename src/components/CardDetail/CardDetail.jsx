9ee2c44966be368edc3537136f8d81ad018db97a
Mateo Ossorio Arana <matossoara@gmail.com>
2023-10-30 15:09:44 -0300

    detail add 2 cart

--- src/components/CardDetail/CardDetail.jsx
+++ src/components/CardDetail/CardDetail.jsx
@@ -6,6 +6,8 @@ import ProductReviews from "../ProductReviews/ProductReviews"
 import { NavLink } from "react-router-dom"
 import { resetReviews } from "../../redux/Reviews/reviewsSlice";
 import {resetDetail } from "../../redux/products/productSlice"
+import { addProductToCart } from "../../redux/Cart/cartActions";
+
 export default function CardDetail() {
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -17,54 +19,66 @@ export default function CardDetail() {
   }, [id]);
 
   const { detail, loading } = useSelector((state) => state.products);
+ 
+  const handleAddClick = () => {
+    if (detail && detail.length === 1) {
+      const product = detail[0];
+      console.log(detail)
+      const imageSrc = product.image; // Accede a la imagen del producto
+      // Resto del código para agregar el producto al carrito
+      dispatch(
+        addProductToCart({
+          id: product.id,
+          product: {
+            imageSrc, // Utiliza la imagen del producto
+            imageAlt: product.title,
+            href: `/Detail/${id}`,
+            title: product.title,
+            price: product.price,
+            stock: product.stock,
+          },
+        })
+      );
+    }
+  }
 
   return (
-    <div className="grid my-[5%] justify-items-center justify-center  ">
-      {loading ? (
-        <p>Loading...</p>
-      ) : detail && detail.length === 1 ? (
-        detail.map((product) => (
-          <div className="flex  border border-gray-900 bg-graym" key={product.id}>
-            {product.image ? (
-              <div className="flex items-center justify-center w-[218px] h-[253px] p-[50px] relative  rounded">
-                <img
-                  className="w-[245px] h-[307px] rounded-tl-lg rounded-tr-xl rounded-bl-xl rounded-br-xl border "
-                  src={product.image}
-                  srcSet={`${product.image} 352w, ${product.image} 832w, ${product.image} 1200w`}
-                  alt="Product Image"
-                />
-          </div>
-            ) : null}
-            <div className="grid grid-cols-1 space-auto place-content-evenly	  text-blackFred-300 p-[50px] items-start	">
-              <span className="flex  ">
-                <p className=" w-[200px] flex font-bayon-bold ">Name:</p>
-                <p className="font-impact">{product.title}</p>
-              </span>
-              <span className="flex  ">                
-                <p className=" w-[200px] flex font-bayon-bold">Category:</p>
-                <p className="font-impact">{product.category}</p>
-              </span>
-              <span className="flex  ">                
-                <p className=" w-[200px] flex font-bayon-bold">Diet:</p>
-                <p className="font-impact">{product.diet}</p>
-              </span>
-              <span className="flex  ">                
-                <p className=" w-[200px] flex font-bayon-bold">
-                  Weight:</p>
-                <p className="font-impact">
-                  {product.weight?.value} {product.weight?.type}
-                </p>
-              </span>
-              <span className="flex ">                
-                <p className=" w-[200px] flex font-bayon-bold">Price: </p>
-                <p className="font-impact">$ {product.price}</p>
-              </span>
-            </div>
-          </div>
-        ))
-      ) : (
-        <p>No product data available.</p>
-      )}
+    <div className="flex flex-col">
+      <div className="grid my-[5%] justify-items-center justify-center">        
+        {loading ? (
+          <p>Loading...</p>
+          ) : detail && detail.length === 1 ? (
+            detail.map((product) => (
+              <div className="flex place-items-center	  border border-gray-900 bg-graym" key={product.id}>
+                {product.image ? (
+                  <div className=" w-[418px] h-[253px] p-[25px] ">
+                    <img
+                      className=" h-[307px] rounded-tl-lg rounded-tr-xl rounded-bl-xl rounded-br-xl border"
+                      src={product.image}
+                      srcSet={`${product.image} 352w, ${product.image} 832w, ${product.image} 1200w`}
+                      alt="Product Image"
+                      />
+                  </div>
+                ) : null}
+                <div className="grid grid-cols-1 space-auto place-content-evenly text-start	  text-blackFred-300 p-[50px] items-start">
+                  <h1 className="font-impact w-[300px] my-[10px]">{product.title}</h1>                  
+                  <h2 className="font-bayon-bold text-[20px] w-[300px] my-[10px]">Flavor:    {product.flavor}</h2>
+                  <h2 className="font-bayon-bold text-[20px] w-[300px] my-[10px]">Ammount:   {product.weight?.value} {product.weight?.type}</h2>
+                  <h2 className="font-bayon-bold text-[20px] w-[300px] my-[10px]">Units available:   {product.stock}</h2>
+                  <h1 className="font-impact w-[300px] my-[10px]">${product.price}</h1>
+                  <button
+                  onClick={handleAddClick}
+                  className="rounded-sm bg-redFred-100 text-whiteFred-100 w-[200px] outline-none hover:text-orangeFred-100"
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            ))
+            ) : (
+          <p>No product data available.</p>
+        )}
+      </div>
         <NavLink to={`/createReview/${id}`}>
           <button>Add review</button>
         </NavLink>
