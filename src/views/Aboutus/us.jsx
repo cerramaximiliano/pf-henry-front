86f5c25153d0f0dea8dbb2ed3930ceb7eef17703
BlancSofia27 <blancsofia27@gmail.com>
2023-10-31 01:12:40 -0300

    responsive about us

--- src/views/Aboutus/us.jsx
+++ src/views/Aboutus/us.jsx
@@ -11,23 +11,20 @@ import sofi from "../../assets/Perfiles/sofi.png"
 export default function Aboutus() {
   return (
     <section class="text-gray-600 body-font">
-      <div class="flex flex-col mt-8 mx- items-center ">
-        <div class="flex flex-col text-center w-full h-64 mx-0 mb-12 relative sm:h-80">
-          <div class="absolute inset-0 bg-[url('https://messagemedia.com/us/wp-content/uploads/sites/4/2020/04/Fitness-header-1.jpg')] blur-sm "></div>
-          <div class="relative z-10 mt-8 sm:mt-2">
-            <h1 class="xl:text-25xl font-bebas mb-4 text-orangeFred-100">
+      <div class="flex flex-col mt-8 mx-0 items-center md:flex-col mx-0 items-center">
+        <div class="flex flex-col text-center w-full h-64 mx-0 mb-16 relative sm:h-80">
+          <div class="absolute inset-0 bg-[url('https://messagemedia.com/us/wp-content/uploads/sites/4/2020/04/Fitness-header-1.jpg')] blur-sm"></div>
+          <div class="xl:flex-col z-10 justify-center lg:mt-10 md:mb-4">
+            <h1 class="xl:text-25xl  lg:text-21xl font-bebas mb-4 text-orangeFred-100 md:mt-4">
               Jenshy Team
             </h1>
-            <p class="xl:w-2/3 mx-auto leading-relaxed font-monse text-orangeFred-200  sm:text-4xl mx-8">
-            Greetings! We are a passionate team of 8 dedicated developers committed to 
-            creating the best online experience for fitness enthusiasts and sports
-             supplement seekers. Together, we blend technology with a love for wellness 
-             to bring you an intuitive and functional platform.
+            <p class="xl:w-70 mx-10 text-orangeFred-100 font-monse">
+            Greetings! We are a passionate team of 8 dedicated developers committed to creating the best online experience for fitness enthusiasts and sports supplement seekers. Together, we blend technology with a love for wellness to bring you an intuitive and functional platform.
             </p>
           </div>
         </div>
 
-        <div class="xl:grid grid-cols-4 gap-16 mb-16 mx-12 ">
+        <div class="xl:grid grid-cols-4 gap-6 mb-16 lg: grid grid-cols-4 gap-6 mb-16 md: grid grid-cols-1 gap-1 mb-2 w-65 ml-20 sm: flex-col mx-0 items-center ">
           {/* lucas card */}
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
@@ -229,7 +226,7 @@ export default function Aboutus() {
 
                           <div class="absolute inset-0 flex flex-col items-center justify-center">
                             <img class="h-16 mt-3" src={iconGit} alt="" />
-                            <h1 class="text-4xl font-bebas text-blackFred-300 mt-0">
+                            <h1 class="text-9xl font-bebas text-blackFred-300 mt-0">
                               JulianBelmartino
                             </h1>
                           </div>
@@ -273,7 +270,7 @@ export default function Aboutus() {
 
                           <div class="absolute inset-0 flex flex-col items-center justify-center">
                             <img class="h-16 mt-3" src={iconGit} alt="" />
-                            <h1 class="text-4xl font-bebas text-blackFred-300 mt-0">
+                            <h1 class="text-9xl font-bebas text-blackFred-300 mt-0">
                               cerramaximiliano
                             </h1>
                           </div>
@@ -361,7 +358,7 @@ export default function Aboutus() {
 
                           <div class="absolute inset-0 flex flex-col items-center justify-center">
                             <img class="h-16 mt-3" src={iconGit} alt="" />
-                            <h1 class="text-4xl font-bebas text-blackFred-300 mt-0">
+                            <h1 class="text-9xl font-bebas text-blackFred-300 mt-0">
                               JuanLeonelPogonza
                             </h1>
                           </div>
